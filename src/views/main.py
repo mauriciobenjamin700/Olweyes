11f62ea52e948e13f179eeb462019cba2107d7ee
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2024-02-20 16:41:08 -0300

    Index compet

--- src/views/main.py
+++ src/views/main.py
@@ -27,6 +27,11 @@ MAIN_BUTTON_HEIGHT = 40
 PRECISION_BUTTON_WIDTH = MAIN_BUTTON_WIDTH//3
 PRECISION_BUTTON_HEIGHT =  MAIN_BUTTON_HEIGHT//2
 
+FONT = join(root,"fonts","Inknut_Antiqua","InknutAntiqua-Bold.ttf")
+BUTTON_COLOR = "#FAFF00"
+OFF_BUTTON_COLOR = "#ABAE0C" 
+#OFF_BUTTON_COLOR = "#FF0000"
+
 
 
 class myPanel(wx.Panel):
@@ -44,14 +49,15 @@ class myPanel(wx.Panel):
         dc.DrawBitmap(image.ConvertToBitmap(), 0, 0)
 
         if not self.buttons:
-            # Cor do botão (amarelo)
-            button_color = "#FAFF00"
+
+            wx.Font.AddPrivateFont(FONT)
         
             # Fonte do botão wx.FONTSTYLE_NORMAL, wx.FONTSTYLE_ITALIC, FONTSTYLE_SLANT
-            main_button_font = wx.Font(11, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_ITALIC, wx.FONTWEIGHT_BOLD)
-            precision_button_font = wx.Font(8, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD)
+            font = wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+            #font_face = font.Fon
             
             # Adiciona os botões se ainda não foram adicionados (ao somar um valor a largura da posição, puxamos para a direita, se subtraimos, puxamos pra esquerda, se somamos na altura, descemnos o valor e vice versa)
+            """
             button_data = [
                 ("Ativar Owlyes",(WINDOW_HEIGHT//2 + int(WINDOW_HEIGHT * 0.30), WINDOW_HEIGHT//2 - int(WINDOW_HEIGHT * 0.10)),(MAIN_BUTTON_WIDTH,MAIN_BUTTON_HEIGHT)), 
                 ("Desativar Owlyes",(WINDOW_HEIGHT//2 + int(WINDOW_HEIGHT * 0.30), WINDOW_HEIGHT//2 + int(WINDOW_HEIGHT * 0.10)),(MAIN_BUTTON_WIDTH,MAIN_BUTTON_HEIGHT)), 
@@ -59,35 +65,137 @@ class myPanel(wx.Panel):
                 ("Normal",(int(WINDOW_WIDTH/1.7) - PRECISION_BUTTON_WIDTH , WINDOW_HEIGHT - 2 * PRECISION_BUTTON_HEIGHT),(PRECISION_BUTTON_WIDTH,PRECISION_BUTTON_HEIGHT)), 
                 ("Atento",(int(WINDOW_WIDTH/1.3) - PRECISION_BUTTON_WIDTH -20, WINDOW_HEIGHT - 2 * PRECISION_BUTTON_HEIGHT),(PRECISION_BUTTON_WIDTH,PRECISION_BUTTON_HEIGHT))]
             print(button_data)
+            """
+            button_data = [
+                ("Ativar Owlyes",(210, 120),(MAIN_BUTTON_WIDTH,MAIN_BUTTON_HEIGHT)), 
+                ("Desativar Owlyes",(210, 180),(MAIN_BUTTON_WIDTH,MAIN_BUTTON_HEIGHT)), 
+                ("Cansado",(202, 260),(PRECISION_BUTTON_WIDTH,PRECISION_BUTTON_HEIGHT)), 
+                ("Normal",(278 , 260),(PRECISION_BUTTON_WIDTH,PRECISION_BUTTON_HEIGHT)), 
+                ("Atento",(353, 260),(PRECISION_BUTTON_WIDTH,PRECISION_BUTTON_HEIGHT))]
+            #print(button_data)
             
             for id,data in enumerate(button_data):
-                button = wx.Button(self, label=f"{data[0]}", pos=data[1],size=data[2])
-                button.SetBackgroundColour(wx.Colour(button_color))
-                
+    
                 match id:
-                    case 0 | 1:
-                
-                        button.Bind(wx.EVT_BUTTON, self.switch_mode)
-                        button.SetBackgroundColour(wx.Colour(255, 255, 0))  # Amarelo
-                        button.SetFont(main_button_font)
+                    case 0:
+                        button = wx.Button(self, label=f"{data[0]}", pos=data[1],size=data[2])
+                        button.Bind(wx.EVT_BUTTON, self.turn_on)
+                        button.SetBackgroundColour(wx.Colour(BUTTON_COLOR))
+                        button.SetFont(font)
+                        button.SetWindowStyleFlag(wx.NO_BORDER)
+                        self.buttons.append(button)
                         
                     case 1:
-                        button.Bind(wx.EVT_BUTTON, self.switch_mode)
-                        button.SetBackgroundColour(wx.Colour(255, 255, 0))  # Amarelo
-                        button.SetFont(main_button_font)
+                        button = wx.Button(self, label=f"{data[0]}", pos=data[1],size=data[2])
+                        button.Bind(wx.EVT_BUTTON, self.turn_off)
+                        button.SetBackgroundColour(wx.Colour(BUTTON_COLOR))
+                        button.SetFont(font)
+                        button.SetWindowStyleFlag(wx.NO_BORDER)
+                        self.buttons.append(button)
+                        
+                    case 2:
+                        toggleButton = wx.ToggleButton(self,label = data[0], pos=data[1],size=data[2])
+                        toggleButton.Bind(wx.EVT_TOGGLEBUTTON,self.mode)
+                        toggleButton.SetBackgroundColour(wx.Colour(OFF_BUTTON_COLOR))
+                        toggleButton.SetFont(font)
+                        button.SetWindowStyleFlag(wx.NO_BORDER)
+                        self.buttons.append(toggleButton)
                     
-                    case 2 | 3 | 4:
-                        button.Bind(wx.EVT_BUTTON, self.switch_mode)
-                        button.SetBackgroundColour(wx.Colour(255, 255, 0))  # Amarelo
-                        button.SetFont(precision_button_font)
+                    case 3:
+                        toggleButton = wx.ToggleButton(self,label = data[0], pos=data[1],size=data[2])
+                        toggleButton.Bind(wx.EVT_TOGGLEBUTTON,self.mode)
+                        toggleButton.SetBackgroundColour(wx.Colour(BUTTON_COLOR))
+                        toggleButton.SetFont(font)
+                        button.SetWindowStyleFlag(wx.NO_BORDER)
+                        self.buttons.append(toggleButton)
+                        
+                    case 4:
+                        toggleButton = wx.ToggleButton(self,label = data[0], pos=data[1],size=data[2])
+                        toggleButton.Bind(wx.EVT_TOGGLEBUTTON,self.mode)
+                        toggleButton.SetBackgroundColour(wx.Colour(OFF_BUTTON_COLOR))
+                        toggleButton.SetFont(font)
+                        button.SetWindowStyleFlag(wx.NO_BORDER)
+                        self.buttons.append(toggleButton)
+                        
                     case _:
-                        button.SetBackgroundColour(wx.Colour(255, 255, 0))  # Amarelo
+                        #button.SetBackgroundColour(wx.Colour(255, 255, 0))  # Amarelo
+                        pass
                     
-                self.buttons.append(button)
                 
-    def turn_on
+                
+    def turn_on(self,event):
+        global state
+        #print("on = ",state)
+        if state == False:
+            self.switch_mode()
+            
+    def turn_off(self,event):
+        global state
+        #print("off = ",state)
+        if state == True:
+            self.switch_mode()
+            
+    
+    def mode(self,event):
+        button = event.GetEventObject()
+        #mode = button.GetValue()
+        label = button.GetLabel()
+        
+        slow,normal, fast = self.buttons[2:5]
+        
+            #event.GetEventObject().SetLabel("Click to Off")
+        match label:
+            case "Cansado":
+                slow.SetBackgroundColour(wx.Colour(BUTTON_COLOR))
+                normal.SetBackgroundColour(wx.Colour(OFF_BUTTON_COLOR))
+                fast.SetBackgroundColour(wx.Colour(OFF_BUTTON_COLOR))
+                
+                slow.SetValue(True)
+                normal.SetValue(True)
+                fast.SetValue(True)
+                
+            case "Normal":
+                slow.SetBackgroundColour(wx.Colour(OFF_BUTTON_COLOR))
+                normal.SetBackgroundColour(wx.Colour(BUTTON_COLOR))
+                fast.SetBackgroundColour(wx.Colour(OFF_BUTTON_COLOR))
+                
+                slow.SetValue(True)
+                normal.SetValue(True)
+                fast.SetValue(True)
+                
+            case "Atento":
+                slow.SetBackgroundColour(wx.Colour(OFF_BUTTON_COLOR))
+                normal.SetBackgroundColour(wx.Colour(OFF_BUTTON_COLOR))
+                fast.SetBackgroundColour(wx.Colour(BUTTON_COLOR))
+                
+                slow.SetValue(True)
+                normal.SetValue(True)
+                fast.SetValue(True)
+                    
+                    
+            #event.GetEventObject().SetBackgroundColour(wx.Colour(BUTTON_COLOR))
+        """
+        else:
+            match label:
+                case "Cansado":
+                    slow.SetBackgroundColour(wx.Colour(OFF_BUTTON_COLOR))
+                    normal.SetBackgroundColour(wx.Colour(BUTTON_COLOR))
+                    fast.SetBackgroundColour(wx.Colour(BUTTON_COLOR))
+                    
+                case "Normal":
+                    slow.SetBackgroundColour(wx.Colour(BUTTON_COLOR))
+                    normal.SetBackgroundColour(wx.Colour(OFF_BUTTON_COLOR))
+                    fast.SetBackgroundColour(wx.Colour(BUTTON_COLOR))
+                case "Atento":
+                    slow.SetBackgroundColour(wx.Colour(BUTTON_COLOR))
+                    normal.SetBackgroundColour(wx.Colour(BUTTON_COLOR))
+                    fast.SetBackgroundColour(wx.Colour(OFF_BUTTON_COLOR))
+            #event.GetEventObject().SetLabel("Click to On")
+            #event.GetEventObject().SetBackgroundColour(wx.Colour(OFF_BUTTON_COLOR))
+            
+        """
 
-    def switch_mode(self, event):
+    def switch_mode(self,event=None):
         global state
         state = not state
         self.Refresh()
