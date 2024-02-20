9a84d9ba68e97335f94878f6c02f559dcf52d94a
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2024-02-20 14:17:51 -0300

    Ajustes na interface gráfica

--- src/models/funcs.py
+++ src/models/funcs.py
@@ -1,15 +1,20 @@
 from pyautogui import locateCenterOnScreen,click,locateOnScreen
 from time import sleep
 from pathlib import Path
+from os.path import join
 
+ROOT = Path.cwd()
+IMAGES = join(ROOT,"images","lol")
 
 def observador(monitorar:bool=True)-> bool:
     """
     Detecta se a fila do League of Legends foi encontrada.
     """
     achei = False
+    target = join(IMAGES,"ENCONTRADA.png")
+    
     while monitorar:
-        locateOnScreen('ENCONTRADA.png',confidence=0.7)
+        locateOnScreen(target,confidence=0.7)
         print('Fila encontrada')
         monitorar = False
         achei = True
@@ -18,9 +23,9 @@ def observador(monitorar:bool=True)-> bool:
     
     return achei
 
-
 def aceitar_fila():
-    aceitar = locateCenterOnScreen('ACEITAR.png',confidence=0.5)
+    target = join(IMAGES,"ACEITAR.png")
+    aceitar = locateCenterOnScreen(target,confidence=0.5)
     click(aceitar.x,aceitar.y)
     sleep(8)
 
--- src/views/main.py
+++ src/views/main.py
@@ -8,13 +8,18 @@ images = join(root, "images")
 #estado da coruja e do bot, Inicia desligado, quando ligado recebe True
 state = False
 
+#image = "images\\background\\0.jpg"
+#image = "images\\background\\1.jpg"
+
 IMAGE_OFF = join(images,"background","0.jpg")
 IMAGE_ON = join(images,"background","1.jpg")
-# TAMANHOS ORIGINAIS
-#WINDOW_WIDTH = 750 
-#WINDOW_HEIGHT = 376
+
+
+# TAMANHOS ORIGINAIS E FINAL
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 300
+#WINDOW_WIDTH = 1280
+#WINDOW_HEIGHT = 720
 
 MAIN_BUTTON_WIDTH = 200
 MAIN_BUTTON_HEIGHT = 40
@@ -22,7 +27,6 @@ MAIN_BUTTON_HEIGHT = 40
 PRECISION_BUTTON_WIDTH = MAIN_BUTTON_WIDTH//3
 PRECISION_BUTTON_HEIGHT =  MAIN_BUTTON_HEIGHT//2
 
-print(PRECISION_BUTTON_WIDTH,PRECISION_BUTTON_HEIGHT)
 
 
 class myPanel(wx.Panel):
@@ -40,19 +44,40 @@ class myPanel(wx.Panel):
         dc.DrawBitmap(image.ConvertToBitmap(), 0, 0)
 
         if not self.buttons:
+            # Cor do botão (amarelo)
+            button_color = "#FAFF00"
+        
+            # Fonte do botão wx.FONTSTYLE_NORMAL, wx.FONTSTYLE_ITALIC, FONTSTYLE_SLANT
+            main_button_font = wx.Font(11, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_ITALIC, wx.FONTWEIGHT_BOLD)
+            precision_button_font = wx.Font(8, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD)
+            
             # Adiciona os botões se ainda não foram adicionados (ao somar um valor a largura da posição, puxamos para a direita, se subtraimos, puxamos pra esquerda, se somamos na altura, descemnos o valor e vice versa)
             button_data = [
                 ("Ativar Owlyes",(WINDOW_HEIGHT//2 + int(WINDOW_HEIGHT * 0.30), WINDOW_HEIGHT//2 - int(WINDOW_HEIGHT * 0.10)),(MAIN_BUTTON_WIDTH,MAIN_BUTTON_HEIGHT)), 
                 ("Desativar Owlyes",(WINDOW_HEIGHT//2 + int(WINDOW_HEIGHT * 0.30), WINDOW_HEIGHT//2 + int(WINDOW_HEIGHT * 0.10)),(MAIN_BUTTON_WIDTH,MAIN_BUTTON_HEIGHT)), 
-                ("Cansado",(int(WINDOW_WIDTH/2.2) - PRECISION_BUTTON_WIDTH , WINDOW_HEIGHT - 2 * PRECISION_BUTTON_HEIGHT),(PRECISION_BUTTON_WIDTH,PRECISION_BUTTON_HEIGHT)), 
-                ("Normal",(int(WINDOW_WIDTH/1.8) - PRECISION_BUTTON_WIDTH , WINDOW_HEIGHT - 2 * PRECISION_BUTTON_HEIGHT),(PRECISION_BUTTON_WIDTH,PRECISION_BUTTON_HEIGHT)), 
-                ("Atento",(int(WINDOW_WIDTH/1.5) - PRECISION_BUTTON_WIDTH -8, WINDOW_HEIGHT - 2 * PRECISION_BUTTON_HEIGHT),(PRECISION_BUTTON_WIDTH,PRECISION_BUTTON_HEIGHT))]
+                ("Cansado",(int(WINDOW_WIDTH/2.3) - PRECISION_BUTTON_WIDTH , WINDOW_HEIGHT - 2 * PRECISION_BUTTON_HEIGHT),(PRECISION_BUTTON_WIDTH,PRECISION_BUTTON_HEIGHT)), 
+                ("Normal",(int(WINDOW_WIDTH/1.7) - PRECISION_BUTTON_WIDTH , WINDOW_HEIGHT - 2 * PRECISION_BUTTON_HEIGHT),(PRECISION_BUTTON_WIDTH,PRECISION_BUTTON_HEIGHT)), 
+                ("Atento",(int(WINDOW_WIDTH/1.3) - PRECISION_BUTTON_WIDTH -20, WINDOW_HEIGHT - 2 * PRECISION_BUTTON_HEIGHT),(PRECISION_BUTTON_WIDTH,PRECISION_BUTTON_HEIGHT))]
             print(button_data)
             
-            for data in button_data:
+            for id,data in enumerate(button_data):
                 button = wx.Button(self, label=f"{data[0]}", pos=data[1],size=data[2])
-                button.Bind(wx.EVT_BUTTON, self.switch_mode)
-                button.SetBackgroundColour(wx.Colour(255, 255, 0))  # Amarelo
+                button.SetBackgroundColour(wx.Colour(button_color))
+                
+                match id:
+                    case 0 | 1:
+                
+                        button.Bind(wx.EVT_BUTTON, self.switch_mode)
+                        button.SetBackgroundColour(wx.Colour(255, 255, 0))  # Amarelo
+                        button.SetFont(main_button_font)
+                    
+                    case 2 | 3 | 4:
+                        button.Bind(wx.EVT_BUTTON, self.switch_mode)
+                        button.SetBackgroundColour(wx.Colour(255, 255, 0))  # Amarelo
+                        button.SetFont(precision_button_font)
+                    case _:
+                        button.SetBackgroundColour(wx.Colour(255, 255, 0))  # Amarelo
+                    
                 self.buttons.append(button)
 
     def switch_mode(self, event):
